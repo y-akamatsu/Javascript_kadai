25bda8251d642a87a42b8b06194d7566a63166a4
y-akamatsu <ak1985tk@gmail.com>
2019-04-14 16:11:37 +0900

    kadai_3

--- html/main.js
+++ html/main.js
@@ -1,21 +1,42 @@
-function getDate() {
-  const date = new Date();
-  const hours = date.getHours();
-  let message = '';
-
-  if (5 <= hours && hours < 12) {
-    message = 'おはようございます';
-  } else if (12 <= hours && hours < 19) {
-    message = 'こんにちわ';
-  } else if (19 <= hours && hours < 24) {
-    message = 'こんばんわ';
-  } else {
-    message = '夜更かしさん';
-  }
-
-  const element = document.getElementById('result');
-  element.innerHTML = message;
+/*
+========== kadai_3 ==========
+*/
+let message = '';
+for( let i = 0; i <= 100; i++){
+  message += `${i} `
 }
+document.getElementById('result').innerHTML = message;
 
-const btn1 = document.getElementById('btn1');
-addEventListener('click', getDate)
+
+/*
+========== kadai_2 ==========
+*/
+
+// function getDate() {
+//   const date = new Date();
+//   const hours = date.getHours();
+//   let message = '';
+
+//   if (5 <= hours && hours < 12) {
+//     message = 'おはようございます';
+//   } else if (12 <= hours && hours < 19) {
+//     message = 'こんにちわ';
+//   } else if (19 <= hours && hours < 24) {
+//     message = 'こんばんわ';
+//   } else {
+//     message = '夜更かしさん';
+//   }
+
+//   const element = document.getElementById('result');
+//   element.innerHTML = message;
+// }
+
+// const btn1 = document.getElementById('btn1');
+// addEventListener('click', getDate)
+
+/*
+========== kadai_1 ==========
+*/
+
+// const element = ducment.getElememntById('result');
+// element.innerHTML = 'こんにちは';
