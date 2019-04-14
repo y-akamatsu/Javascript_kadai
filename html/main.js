13172707827b5aefc0f43c3aaf754ea0ecdf0cb4
y-akamatsu <ak1985tk@gmail.com>
2019-04-14 15:23:31 +0900

    kadai1

--- html/main.js
+++ html/main.js
@@ -1,2 +1,21 @@
-const element = document.getElementById("result")
-element.innerHTML = "こんにちわ"
+function getDate() {
+  const date = new Date();
+  const hours = date.getHours();
+  let message = '';
+
+  if (5 <= hours && hours < 12) {
+    message = 'おはようございます';
+  } else if (12 <= hours && hours < 19) {
+    message = 'こんにちわ';
+  } else if (19 <= hours && hours < 24) {
+    message = 'こんばんわ';
+  } else {
+    message = '夜更かしさん';
+  }
+
+  const element = document.getElementById('result');
+  element.innerHTML = message;
+}
+
+const btn1 = document.getElementById('btn1');
+addEventListener('click', getDate)
